33028fd1852d9d65ce18978508ba78b8ce6795c6
Omar Alesharie <omar.alesharie@gmail.com>
2019-04-29 08:35:17 +0300

    Done
    Simple Bug Fixed

--- TPC-With-Cube/Assets/Scripts/CameraController.cs
+++ TPC-With-Cube/Assets/Scripts/CameraController.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-    public float rotationSpeed = 1;
     public float Away;
     public float Up;
     public float cameraSmothFollow;
 
     [SerializeField]
     private Vector2 verticalViewLimits = new Vector2(-30, 40);
+    [SerializeField]
+    private float RotationSpeed = 3;
 
     private Transform Player;
     private Transform rootOfPlayer;
     private Vector3 Target;
     private Transform TargetTransform;
     private float mouseX, mouseY;
-    private float StartedmouseX, StartedmouseY;
+    private float LastMouseX, LastMouseY;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,12 +33,11 @@ public class CameraController : MonoBehaviour
         TargetTransform = new GameObject("TargetTransform").transform;
 
         SetTargetPosition();
-
-        transform.position = Vector3.Lerp(transform.position, TargetTransform.position, cameraSmothFollow * Time.deltaTime);
+        rootOfPlayer.forward = Player.forward;
         transform.LookAt(Player.position);
 
-        StartedmouseX = Input.GetAxis("Mouse X");
-        StartedmouseY = Input.GetAxis("Mouse Y");
+        LastMouseX = Input.GetAxis("Mouse X");
+        LastMouseY = Input.GetAxis("Mouse Y");
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -54,19 +56,22 @@ public class CameraController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Q))
         {
-            mouseX = 0f;
-            mouseY = 0f;
-            rootOfPlayer.rotation = Quaternion.Euler(Player.rotation.x, Player.rotation.y, 0f);
+            rootOfPlayer.forward = Player.forward;
         }
 
         mouseY = Mathf.Clamp(mouseY, verticalViewLimits.x, verticalViewLimits.y);
 
-        if (mouseX-StartedmouseX != Mathf.Epsilon || mouseY-StartedmouseY != Mathf.Epsilon)
+        if (mouseX - LastMouseX != 0 || mouseY - LastMouseY != 0)
         {
-            rootOfPlayer.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+            //rootOfPlayer.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+            rootOfPlayer.Rotate(Vector3.up, (mouseX - LastMouseX) * RotationSpeed);
+            rootOfPlayer.Rotate(Vector3.right, (mouseY - LastMouseY) * RotationSpeed);
+
+            LastMouseX = mouseX;
+            LastMouseY = mouseY;
         }
-        
-        rootOfPlayer.SetPositionAndRotation(Player.position, rootOfPlayer.rotation);
+
+        rootOfPlayer.position = Player.position;
 
         SetTargetPosition();
         WallCollision();
@@ -77,10 +82,9 @@ public class CameraController : MonoBehaviour
 
     private void WallCollision()
     {
-        RaycastHit Hit;
-        if (Physics.Linecast(rootOfPlayer.position, TargetTransform.position, out Hit))
+        if (Physics.Linecast(rootOfPlayer.position, TargetTransform.position, out RaycastHit Hit))
         {
-            TargetTransform.position = new Vector3(Hit.point.x, TargetTransform.position.y,Hit.point.z);
+            TargetTransform.position = new Vector3(Hit.point.x, TargetTransform.position.y, Hit.point.z);
         }
     }
 }
